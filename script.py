d72fbbbeb66685bb83d3ea7285dfbd314b61c778
mohamedcherif <amgharchrif@gmail.com>
2020-05-12 13:18:28 +0200

    Update script.py
    change the script

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
-
+#import numpy as np
+#import math
+#from random import randrange
+#from tkinter import* 
+#import matplotlib
+#matplotlib.use('TkAgg')
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import axes3d
